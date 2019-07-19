f41a7cb7a5b264781e86c92b41bc32273f27d451
rly101112 <rly101112@163.com>
2019-07-19 13:51:14 +0800

    CRUD

--- rlySpringMVC4/WebContent/index.jsp
+++ rlySpringMVC4/WebContent/index.jsp
@@ -8,6 +8,24 @@
 </head>
 <body>
 <h1>hello SpringMVC</h1>
+<script type="text/javascript" src="scripts/jquery-1.9.1.min.js"></script>
+<script type="text/javascript">
+	$(function(){
+		$("#testJson").click(function(){
+			var url = this.href;
+			var args = {};
+			$.post(url, args, function(data){
+				for(var i = 0; i < data.length; i++){
+					var id = data[i].id;
+					var lastName = data[i].lastName;
+					
+					alert(id + ": " + lastName);
+				}
+			});
+			return false;
+		});
+	})
+</script>
 
 	<a href="test2">Test RequestMapping</a>
 	<br>
@@ -79,6 +97,43 @@
 	
 	<a href="emps">获取所有数据</a>
 	<br><br>
+	
+	
+		
+	<a href="testJson" id="testJson">测试 Json</a>
+	<br><br>
+	
+	<h4>测试Converter</h4>
+		<form action="testHttpMessageConverter" method="POST" enctype="multipart/form-data">
+		File: <input type="file" name="file"/>
+		Desc: <input type="text" name="desc"/>
+		<input type="submit" value="Submit"/>
+	</form>
+	
+	<a href="testResponseEntity">Test ResponseEntity</a>
+	
+	
+	<!--  
+		关于国际化:
+		1. 在页面上能够根据浏览器语言设置的情况对文本(不是内容), 时间, 数值进行本地化处理
+		2. 可以在 bean 中获取国际化资源文件 Locale 对应的消息
+		3. 可以通过超链接切换 Locale, 而不再依赖于浏览器的语言设置情况
+		
+		解决:
+		1. 使用 JSTL 的 fmt 标签
+		2. 在 bean 中注入 ResourceBundleMessageSource 的示例, 使用其对应的 getMessage 方法即可
+		3. 配置 LocalResolver 和 LocaleChangeInterceptor
+	-->	
+	<br><br>
+	<a href="i18n">I18N PAGE</a>
+	
+	<h1>文件上传</h1>
+	<form action="testFileUpload" method="POST" enctype="multipart/form-data">
+		File: <input type="file" name="file"/>
+		Desc: <input type="text" name="desc"/>
+		<input type="submit" value="Submit"/>
+	</form>
+	
 	
 </body>
 </html>
--- rlySpringMVC4/src/com/renly/test/SpringMVCTest.java
+++ rlySpringMVC4/src/com/renly/test/SpringMVCTest.java
@@ -1,12 +1,28 @@
  package com.renly.test;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.renly.CRUD.dao.EmployeeDao;
 import com.renly.entities.Employee;
@@ -31,6 +47,62 @@ import com.renly.entities.Employee;
          return "redirect:/emps";
      }
      
-    
+     /*
+      * 测试处理json
+      */
+     @ResponseBody
+     @RequestMapping("/testJson")
+     public Collection<Employee> testJson(){
+         return employeeDao.getAll();
+     }
 
+     /*
+      * 测试Converter
+      */
+     @ResponseBody
+     @RequestMapping("/testHttpMessageConverter")
+     public String testHttpMessageConverter(@RequestBody String body){
+         System.out.println(body);
+         return "helloworld! " + new Date();
+     }
+     
+     
+     @RequestMapping("/testResponseEntity")
+     public ResponseEntity<byte[]> testResponseEntity(HttpSession session) throws IOException{
+         byte [] body = null;
+         ServletContext servletContext = session.getServletContext();
+         InputStream in = servletContext.getResourceAsStream("/files/abc.txt");
+         body = new byte[in.available()];
+         in.read(body);
+         
+         HttpHeaders headers = new HttpHeaders();
+         headers.add("Content-Disposition", "attachment;filename=abc.txt");
+         
+         HttpStatus statusCode = HttpStatus.OK;
+         
+         ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(body, headers, statusCode);
+         return response;
+     }
+     
+     /*
+      * 测试国际化
+      */
+      @RequestMapping("/i18n")
+     public String testI18n(Locale locale){
+//         String val = messageSource.getMessage("i18n.user", null, locale);
+//         System.out.println(val); 
+         return "i18n";
+     }
+      /*
+       * 文件上传
+       */
+      @RequestMapping("/testFileUpload")
+      public String testFileUpload(@RequestParam("desc") String desc, 
+              @RequestParam("file") MultipartFile file) throws IOException{
+          System.out.println("desc: " + desc);
+          System.out.println("OriginalFilename: " + file.getOriginalFilename());
+          System.out.println("InputStream: " + file.getInputStream());
+          file.transferTo(new File("d:\\"+file.getOriginalFilename()));
+          return "success";
+      }
 }
